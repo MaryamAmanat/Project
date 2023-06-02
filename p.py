6b1473d44e44bf25749f76dfaaf950b347960a9e
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-06-02 17:40:07 +0500

    Create p.py

---
+++ p.py
@@ -0,0 +1,14 @@
+import plotly.graph_objects as go  # pip install plotly
+import streamlit as st  # pip install streamlit
+
+# -------------- SETTINGS --------------
+incomes = ["Salary", "Blog", "Other Income"]
+expenses = ["Rent", "Utilities", "Groceries", "Car", "Other Expenses", "Saving"]
+currency = "USD"
+page_title = "Income and Expense Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+# --------------------------------------
+
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_title + " " + page_icon)
