6914deaa89ab8e0726657fdf6f2e5d11b8c8277e
MaryamAmanat <113723249+MaryamAmanat@users.noreply.github.com>
2023-06-02 18:17:17 +0500

    Update pp.py

--- pp.py
+++ pp.py
@@ -1,46 +1,93 @@
+import os
 import streamlit as st
-import plotly.graph_objects as go
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def generate_expense_report(expenses):
-    total_expenses = sum(expenses)
-    average_expense = total_expenses / len(expenses)
+# Create data directory if it doesn't exist
+if not os.path.exists("data"):
+    os.makedirs("data")
 
-    st.write("Expense Report")
-    st.write("--------------------")
-    st.write("Total Expenses: $", total_expenses)
-    st.write("Average Expense: $", average_expense)
-    st.write("--------------------")
-    st.write("Individual Expenses:")
-    for index, expense in enumerate(expenses, start=1):
-        st.write(f"Expense {index}: $", expense)
+def data_entry():
+    st.subheader("Data Entry")
 
-def generate_expense_chart(expenses):
-    expense_labels = [f"Expense {i+1}" for i in range(len(expenses))]
+    # Select month and date
+    month = st.selectbox("Select Month", ["January", "February", "March", "April", "May", "June", "July",
+                                          "August", "September", "October", "November", "December"])
+    date = st.date_input("Select Date")
 
-    fig = go.Figure(data=[go.Bar(x=expense_labels, y=expenses)])
-    fig.update_layout(title="Expense Distribution", xaxis_title="Expenses", yaxis_title="Amount")
-    st.plotly_chart(fig)
+    # Create filename based on month and date
+    filename = f"data/{month}_{date.day}.csv"
+
+    # Check if file already exists, create new file if not
+    if not os.path.isfile(filename):
+        with open(filename, "w") as f:
+            f.write("Income,Type,Expense,Comments\n")
+            st.success(f"New file created: {filename}")
+
+    # Take income details
+    st.subheader("Income Details")
+    income = st.number_input("Income Amount (USD)", min_value=0.0, step=0.01)
+    income_type = st.selectbox("Income Type", ["Salary", "Blog", "Other Income"])
+
+    # Take expense details
+    st.subheader("Expense Details")
+    expense = st.number_input("Expense Amount (USD)", min_value=0.0, step=0.01)
+    expense_type = st.selectbox("Expense Type", ["Grocery", "Utility", "Car", "Rent", "Other"])
+    comments = st.text_area("Comments")
+
+    # Append data to the file
+    with open(filename, "a") as f:
+        f.write(f"{income},{income_type},{expense},{expense_type},{comments}\n")
+    st.success("Data entry added successfully!")
+
+def data_visualization():
+    st.subheader("Data Visualization")
+
+    # Read all the CSV files in the 'data' directory
+    csv_files = [file for file in os.listdir("data") if file.endswith(".csv")]
+
+    if csv_files:
+        # Combine all the CSV data into a single DataFrame
+        df = pd.concat([pd.read_csv(os.path.join("data", file)) for file in csv_files])
+
+        # Group the data by income type and calculate the total income
+        grouped_income = df.groupby("Type").sum().reset_index()
+
+        # Create a bar chart for income
+        plt.figure(figsize=(10, 6))
+        plt.bar(grouped_income["Type"], grouped_income["Income"])
+        plt.xlabel("Income Type")
+        plt.ylabel("Total Income (USD)")
+        plt.title("Income by Type")
+        st.pyplot(plt)
+
+        # Group the data by expense type and calculate the total expense
+        grouped_expense = df.groupby("Expense").sum().reset_index()
+
+        # Create a bar chart for expense
+        plt.figure(figsize=(10, 6))
+        plt.bar(grouped_expense["Expense"], grouped_expense["Expense"])
+        plt.xlabel("Expense Type")
+        plt.ylabel("Total Expense (USD)")
+        plt.title("Expense by Type")
+        st.pyplot(plt)
+
+        # Show the raw data table
+        st.subheader("Raw Data")
+        st.dataframe(df)
+    else:
+        st.warning("No data files found.")
 
 def main():
-    st.title("Expense Report Generator")
-    st.write("Enter your expenses below:")
+    st.title("Expense Tracker App")
+    st.write("Welcome to the Expense Tracker App!")
 
-    
-    expense_list = []
+    option = st.sidebar.selectbox("Select Option", ["Data Entry", "Data Visualization"])
 
-    while True:
-        expense = st.number_input("Expense:", value=0.0, step=0.01)
-        expense_list.append(expense)
-
-        add_more = st.button("Add More Expenses")
-        if not add_more:
-            break
-
-    if expense_list:
-        generate_expense_report(expense_list)
-        generate_expense_chart(expense_list)
-    else:
-        st.write("No expenses entered.")
+    if option == "Data Entry":
+        data_entry()
+    elif option == "Data Visualization":
+        data_visualization()
 
 if __name__ == '__main__':
     main()
